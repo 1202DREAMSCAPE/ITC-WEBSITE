874ae955fe5f7d9f2801ae64fb9610a300693cfe
Seajay <88035813+1202DREAMSCAPE@users.noreply.github.com>
2022-01-16 21:00:37 +0800

    Update privacyform-process.php

--- WEB_HTML/PHP/privacyform-process.php
+++ WEB_HTML/PHP/privacyform-process.php
@@ -25,7 +25,7 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "seajay.crz@gmail.com";
+$EmailTo = "emailhere";
 $Subject = "New privacy request from Aria landing page";
 
 // prepare email body text
